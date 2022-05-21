a89b49f40d463087a08c277c5033130a51d14255
Al Camara <Alhousseinec@gmail.com>
2022-05-21 02:11:02 -0500

    imported express and body-parser modules and enable listener

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.listen(PORT,()=>{
+    console.log("I'm listen 👂");
+})
