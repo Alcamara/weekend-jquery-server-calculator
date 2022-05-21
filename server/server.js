5ee8a5b1d01839c37d1584af88e9f8dcad7adf45
Al Camara <Alhousseinec@gmail.com>
2022-05-21 02:56:00 -0500

    created calculation object and methods

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+//const calculation = require('./calculation')
+
 app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,6 +16,7 @@ app.listen(PORT,()=>{
 
 app.get('/results',(req,res)=>{
     console.log('step 1: sending data');
+    res.send({num:4})
     res.sendStatus(200)
 })
 
